9599676cf75232dee943337f638cc2a41115fa4a
Mariana Orico <marianaorico@gmail.com>
2022-07-28 21:46:52 -0300

    Alguns alterações

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,13 @@ import './footer.css'
 
 function Footer() {
     return (
-        <>
-            <footer className='footer'>
-                <div className='footer-div'>
-                <h1>Projeto Final da Turma On17 - Front-End da Reprograma</h1>
-                </div>
-                <div className='social-media'>
-                    <a href='https://github.com/MarianaOrico'><img className='first' src='./../../../github-icon.png' /></a>
-                    <a href='https://www.linkedin.com/in/marianaorico/'><img className='second' src='./../../../linkedIn-icon-removebg-preview.png' /></a>
-                </div>
-            </footer>
-        </>
+        <footer className='footer'>
+            <h1>Projeto Final da Turma On17 - Front-End da Reprograma</h1>
+            <div>
+                <a href='https://github.com/MarianaOrico'><img className='first' src='./../../../github-icon.png' /></a>
+                <a href='https://www.linkedin.com/in/marianaorico/'><img className='second' src='./../../../linkedIn-icon-removebg-preview.png' /></a>
+            </div>
+        </footer>
     )
 }
 
